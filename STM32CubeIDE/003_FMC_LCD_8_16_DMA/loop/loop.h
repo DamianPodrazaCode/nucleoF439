f93e1460b061d20144ab0bfc712f4c12fd02f2f9
DamianPodrazaCode <damian.podraza@gmail.com>
2023-03-12 22:33:16 +0100

    Signed-off-by: DamianPodrazaCode <damian.podraza@gmail.com>

---
+++ STM32CubeIDE/003_FMC_LCD_8_16_DMA/loop/loop.h
@@ -0,0 +1,17 @@
+/*
+ * loop.h
+ *
+ *  Created on: Mar 9, 2023
+ *      Author: ZoMbiE
+ */
+
+#ifndef LOOP_H_
+#define LOOP_H_
+
+#include "main.h"
+#include "dma.h"
+
+void setup();
+void loop();
+
+#endif /* LOOP_H_ */
