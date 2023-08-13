a51ac0458f2c77d81a983a2f16f50bddf90051b1
DamianPodrazaCode <damian.podraza@gmail.com>
2023-08-13 20:23:43 +0200

    Signed-off-by: DamianPodrazaCode <damian.podraza@gmail.com>

--- STM32CubeIDE/020_SDRAM_MT48LC16M16A2-76_test/Core/Src/main.c
+++ STM32CubeIDE/020_SDRAM_MT48LC16M16A2-76_test/Core/Src/main.c
@@ -2,14 +2,14 @@
 // adres start 0xC0000000 - adres stop 0xc1ffffff - 64Mb, 8MB, 4M half Word, 2M Word
 #define SDRAM_BANK_ADDR ((uint32_t)0xC0000000)
 
-
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
+static void GPIO_Init(void);
 static void MX_FMC_Init(void);
+
 int main(void) {
 	HAL_Init();
 	SystemClock_Config();
-	MX_GPIO_Init();
+	GPIO_Init();
 	MX_FMC_Init();
 
 	// czyszczenie całej pamięci SDRAM
@@ -133,7 +133,7 @@ static void MX_FMC_Init(void) {
 	HAL_SDRAM_ProgramRefreshRate(&hsdram1, 683);
 }
 
-static void MX_GPIO_Init(void) {
+static void GPIO_Init(void) {
 	__HAL_RCC_GPIOF_CLK_ENABLE();
 	__HAL_RCC_GPIOH_CLK_ENABLE();
 	__HAL_RCC_GPIOC_CLK_ENABLE();
@@ -142,4 +142,86 @@ static void MX_GPIO_Init(void) {
 	__HAL_RCC_GPIOD_CLK_ENABLE();
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 	__HAL_RCC_GPIOB_CLK_ENABLE();
+
+	__HAL_RCC_FMC_CLK_ENABLE();
+	/** FMC GPIO Configuration
+	 PF0   ------> FMC_A0
+	 PF1   ------> FMC_A1
+	 PF2   ------> FMC_A2
+	 PF3   ------> FMC_A3
+	 PF4   ------> FMC_A4
+	 PF5   ------> FMC_A5
+	 PC0   ------> FMC_SDNWE
+	 PC2   ------> FMC_SDNE0
+	 PC3   ------> FMC_SDCKE0
+	 PF11   ------> FMC_SDNRAS
+	 PF12   ------> FMC_A6
+	 PF13   ------> FMC_A7
+	 PF14   ------> FMC_A8
+	 PF15   ------> FMC_A9
+	 PG0   ------> FMC_A10
+	 PG1   ------> FMC_A11
+	 PE7   ------> FMC_D4
+	 PE8   ------> FMC_D5
+	 PE9   ------> FMC_D6
+	 PE10   ------> FMC_D7
+	 PE11   ------> FMC_D8
+	 PE12   ------> FMC_D9
+	 PE13   ------> FMC_D10
+	 PE14   ------> FMC_D11
+	 PE15   ------> FMC_D12
+	 PD8   ------> FMC_D13
+	 PD9   ------> FMC_D14
+	 PD10   ------> FMC_D15
+	 PD14   ------> FMC_D0
+	 PD15   ------> FMC_D1
+	 PG2   ------> FMC_A12
+	 PG4   ------> FMC_BA0
+	 PG5   ------> FMC_BA1
+	 PG8   ------> FMC_SDCLK
+	 PD0   ------> FMC_D2
+	 PD1   ------> FMC_D3
+	 PG15   ------> FMC_SDNCAS
+	 PE0   ------> FMC_NBL0
+	 PE1   ------> FMC_NBL1
+	 */
+
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+	GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_11 | GPIO_PIN_12
+			| GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+	GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
+	HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+	GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_2 | GPIO_PIN_3;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+	GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+	GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_8 | GPIO_PIN_15;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+	GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
+	HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+	GPIO_InitStruct.Pin = GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14
+			| GPIO_PIN_15 | GPIO_PIN_0 | GPIO_PIN_1;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+	GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
+	HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+	GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_14 | GPIO_PIN_15 | GPIO_PIN_0 | GPIO_PIN_1;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+	GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
+	HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
 }
